27c31f70c062a97c1f87f0bc57de9ef3b3b47e81
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-10-23 17:13:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_of_Weeka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] dayOfWeek = { "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday" };
+            
+            if (n >= 1 && n <= 7)
+            {
+                Console.WriteLine(dayOfWeek[n - 1]);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+        }
+    }
+}
